c3cb36b42f7cda00a47cad6716b3bd9df513fd46
Nicolaas <github@sunnysideup.co.nz>
2018-03-20 10:49:32 +1300

    MINOR: nicer formatting

--- Git_Tag_Change_Composer_Update.php
+++ Git_Tag_Change_Composer_Update.php
@@ -4,11 +4,13 @@
 //Check for user version tag argument
 $userDefinedTag = "";
 
+$dir = '/container/application/public';
+
 //Name of file that checkout logs will be dumped into.
 $logFileName = "/container/application/gitCheckoutLog.txt";
 
 //!!!!!! UNCOMENT FOR LIVE ENVIRONMENT !!!!!!
-chdir('/container/application/public');
+chdir($dir);
 
 //If present sets the desired tag version from commandline
 if($argc > 1){
@@ -17,7 +19,11 @@ if($argc > 1){
 
 //Get the most recent details from the repo then
 //Fetch the tag details and turn them into array.
-echo shell_exec('pwd');
+$pwd = shell_exec('pwd');
+if($pwd !== $dir) {
+    echo 'You are not in the right directory: '.$dir."\n";
+    exit();
+}
 shell_exec('git fetch --all');
 
 $list = (shell_exec('git tag'));
@@ -27,15 +33,23 @@ $splitList = explode("\n",$list);
 
 array_pop($splitList);
 
+$tagToPull = '';
+if(count($splitList) === 0) {
+    $tagToPull = 'master';   
+}
+
 //Check if the user specified tag is present otherwise
 //pull the most recent.
-$tagToPull;
-if(in_array($userDefinedTag, $splitList)){
+if($tagToPull === 'master') {
+     echo 'No tags available, using: ' . $tagToPull . "\n";
+    //do nothing
+}
+elseif(in_array($userDefinedTag, $splitList)){
     $tagToPull = $userDefinedTag;
-    echo 'Tag found, using specified tag: ' . $tagToPull ." \n";
+    echo 'Tag found, using specified tag: ' . $tagToPull ."\n";
 } else {
     $tagToPull = end($splitList);
-    echo 'Requested tag not found, using latest tag: ' . $tagToPull . " instead. \n";
+    echo 'Requested tag not found, using latest tag: ' . $tagToPull . " instead.\n";
 }
 
 //Proceed to checkout the last
@@ -44,9 +58,17 @@ shell_exec('git checkout '. $tagToPull);
 //Composer install all the correct dependancies.
 shell_exec('composer install --no-dev');
 
+//just in case ...
+
+//remove _dev folder
+shell_exec('rm _dev -rf');
+//remove databases
+shell_exec('find . -name "*.sql.gz" -exec rm "{}" \;');
+shell_exec('find . -name "*.sql" -exec rm "{}" \;');
+
 //Write to the log
 $oldFileContent = file_get_contents($logFileName);
-$newFileContent = $oldFileContent . "\nChecked out tag: ". $tagToPull . " at ". date("Y-m-d H:i:s");
+$newFileContent = $oldFileContent . "\nChecked out tag: ". $tagToPull . " at ". date("Y-m-d H:i");
 file_put_contents($logFileName, $newFileContent);
 
 echo '

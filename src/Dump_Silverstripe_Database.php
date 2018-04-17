d7207e4c6e889c54bf3a048dc76fe1be416de963
Nicolaas <github@sunnysideup.co.nz>
2018-04-17 19:18:24 +1200

    Update Dump_Silverstripe_Database.php

--- src/Dump_Silverstripe_Database.php
+++ src/Dump_Silverstripe_Database.php
@@ -7,6 +7,7 @@ if((php_sapi_name() === 'cli')) {
     require_once($location);
 
     $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > /container/application/'.SS_DATABASE_NAME.'.sql';
+    var_dump(shell_exec($command));
     if(file_exists('/container/application/'.SS_DATABASE_NAME.'.sql')) {
         echo '---------- DUMPED ------------';
     } else {

35b09dcecbb72368ebd191ee0b852599120c9ef1
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-05-01 10:29:52 +1200

    MAJOR: new version

--- src/Git_Tag_Change_Composer_Update.php
+++ src/Git_Tag_Change_Composer_Update.php
@@ -1,42 +1,84 @@
 <?php
+
 //!!!!!!!!!! WARNING THIS DOES NOT YET ACCOUNT FOR REPOS WITHOUT TAGS!!!!!!!!!!!!!!!!!!!
 if((php_sapi_name() === 'cli')) {
     //Check for user version tag argument
-    $userDefinedTag = "";
+    $userDefinedTag = '';
 
-    $dir = '/container/application/public';
+    //find the safe dir ...
+    $safeDir = dirname(realpath($argv[0]));
+    $found = false;
+    while($found === false && file_exists($safeDir)) {
+        if(file_exists('.htaccess')) {
+            //we are in public territory... NOT GOOD
+        }
+        elseif(file_exists($safeDir.'/_ss_environment.php')) {
+            $found = true;
+        }
+        elseif(file_exists($safeDir.'/.env')) {
+            $found = true;
+        }
+        if($found === false)
+            $safeDir = dirname($safeDir);
+        }
+    }
+
+    //now find the public directory ...
+    $options [
+        'public',
+        'wwww',
+        'public_html'
+    ];
+
+    foreach($options as $option) {
+        if(file_exists($safeDir.'/'.$option)) {
+            $publicDir = $safeDir.'/'.$option;
+            break;
+        }
+    }
+
 
     //Name of file that checkout logs will be dumped into.
-    $logFileName = "/container/application/_git_log.txt";
+    $logFileName = "_git_log.txt";
 
-    //!!!!!! UNCOMENT FOR LIVE ENVIRONMENT !!!!!!
-    chdir($dir);
 
     //If present sets the desired tag version from commandline
     if($argc > 1){
         $userDefinedTag = $argv[1];
     }
 
+    ########## ########## ##########
+    ########## START ACTION
+    ########## ########## ##########
+    echo '
+========================
+';
+    echo 'SAFE DIR: '.$safeDir;
+    echo 'PUBLIC DIR: '.$safeDir;
+    echo '
+========================
+';
+
+    //!!!!!! UNCOMENT FOR LIVE ENVIRONMENT !!!!!!
+    chdir($publicDir);
+
     //Get the most recent details from the repo then
     //Fetch the tag details and turn them into array.
     $pwd = trim(shell_exec('pwd'));
-    if($pwd !== $dir) {
-        echo 'You are not in the right directory: '.$dir."\n";
+    if($pwd !== $publicDir) {
+        echo 'You are not in the right directory: '.$publicDir."\n";
         exit();
     }
     shell_exec('git fetch --tags');
     shell_exec('git fetch --all');
 
+    //what tag are we getting ....
     $list = (shell_exec('git tag'));
     $splitList = explode("\n",$list);
-
-    //var_dump($splitList); for debugging
-
     array_pop($splitList);
-
     $tagToPull = '';
     if(count($splitList) === 0) {
-        $tagToPull = 'master';   
+        $tagToPull = 'master';
     }
 
     //Check if the user specified tag is present otherwise
@@ -56,6 +98,11 @@ if((php_sapi_name() === 'cli')) {
     //Proceed to checkout the last
     shell_exec('git checkout '. $tagToPull);
 
+    //Write to the log
+    $oldFileContent = file_get_contents($safeDir.'/'.$logFileName);
+    $newFileContent = $oldFileContent . "\nChecked out tag: ". $tagToPull . " at ". date("Y-m-d H:i");
+    file_put_contents($safeDir.'/'.$logFileName, $newFileContent);
+
     //Composer install all the correct dependancies.
     shell_exec('composer install --no-dev');
 
@@ -63,18 +110,17 @@ if((php_sapi_name() === 'cli')) {
 
     //remove _dev folder
     shell_exec('rm _dev -rf');
+    //remove READMEs
+    shell_exec('rm README.md');
     //remove databases
     shell_exec('find . -name "*.sql.gz" -exec rm "{}" \;');
     shell_exec('find . -name "*.sql" -exec rm "{}" \;');
 
-    //Write to the log
-    $oldFileContent = file_get_contents($logFileName);
-    $newFileContent = $oldFileContent . "\nChecked out tag: ". $tagToPull . " at ". date("Y-m-d H:i");
-    file_put_contents($logFileName, $newFileContent);
+    echo <<<printme
 
-    echo '
-    ========================
-    You are now at '.shell_exec('git describe --tags ').'
-    ========================
-    ';
+========================
+You are now at '.shell_exec('git describe --tags ').'
+========================
+
+printme;
 }

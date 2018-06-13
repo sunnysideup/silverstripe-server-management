81c854a85c507dceb194fe63ab4a6ead5f37ae65
Nicolaas <github@sunnysideup.co.nz>
2018-06-13 15:51:14 +1200

    Update IMPORT_DB.php

--- src/IMPORT_DB.php
+++ src/IMPORT_DB.php
@@ -3,13 +3,23 @@ if ((php_sapi_name() === 'cli')) {
     $location =  '/container/application/_ss_environment.php';
     
     require_once($location);
-    $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > /container/application/'.SS_DATABASE_NAME.'.sql';
-    var_dump(shell_exec($command));
-    if (file_exists('/container/application/'.SS_DATABASE_NAME.'.sql')) {
-        echo '---------- DUMPED ------------';
-    } else {
-        echo '---------- FAILED ------------';
+    $message   =  "Are you sure you want to do import a database [y/N]";
+    print $message;
+    flush();
+    ob_flush();
+    $confirmation  =  trim( fgets( STDIN ) );
+    if ( $confirmation !== 'y' ) {
+       // The user did not say 'y'.
+       exit (0);
+    } else {   
+        $command = 'mysql -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' < /container/application/'.SS_DATABASE_NAME.'.sql';
+        var_dump(shell_exec($command));
+        if (file_exists('/container/application/'.SS_DATABASE_NAME.'.sql')) {
+            echo '---------- DUMPED ------------';
+        } else {
+            echo '---------- FAILED ------------';
+        }
+        echo '
+        ';
     }
-    echo '
-    ';
 }

bd3fbe1ab8462e9e7a1533bc91217abc78ce7a9b
Nicolaas <github@sunnysideup.co.nz>
2018-06-13 15:56:18 +1200

    Update IMPORT_DB.php

--- src/IMPORT_DB.php
+++ src/IMPORT_DB.php
@@ -3,14 +3,15 @@ if ((php_sapi_name() === 'cli')) {
     $location =  '/container/application/_ss_environment.php';
     
     require_once($location);
+    $command = 'mysql -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' < /container/application/'.SS_DATABASE_NAME.'.sql';
     $message   =  "Are you sure you want to do import a database [y/N]";
+    print '# '.$command;
     print $message;
     $confirmation  =  trim( fgets( STDIN ) );
     if ( $confirmation !== 'y' ) {
        // The user did not say 'y'.
        exit (0);
     } else {   
-        $command = 'mysql -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' < /container/application/'.SS_DATABASE_NAME.'.sql';
         var_dump(shell_exec($command));
         if (file_exists('/container/application/'.SS_DATABASE_NAME.'.sql')) {
             echo '---------- DUMPED ------------';

61def2344ccd7d79d998ae11229fd5d15a3e1c09
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-08-01 13:47:57 +1200

    FIXES

--- src/SSPAK.php
+++ src/SSPAK.php
@@ -7,36 +7,36 @@ if ((php_sapi_name() === 'cli')) {
     $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > '.$publicDir.'/database.sql';
     echo '# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
-    if (file_exists('/container/application/database.sql')) {
+    if (file_exists($publicDir.'/database.sql')) {
         echo '---------- DUMPED ------------';
     } else {
         echo '---------- FAILED ------------';
     }
     if(file_exists($publicDir.'/public/assets')) {
         $command = 'cd '.$publicDir.' && tar czf assets.tar.gz public/assets';
-        echo '# '.$command.PHP_EOL;
+        echo PHP_EOL.'# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
     elseif(file_exists($publicDir.'/assets')) {
         $command = 'cd '.$publicDir.' && tar czf assets.tar.gz assets';
-        echo '# '.$command.PHP_EOL;
+        echo PHP_EOL.'# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
 
     $command = 'cd '.$publicDir.' && tar cf '.SS_DATABASE_NAME.'.sspak assets.tar.gz database.sql.gz';
-    echo '# '.$command.PHP_EOL;
+    echo PHP_EOL.'# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
 
     $command = 'rm '.$publicDir.'/database.sql.gz';
-    echo '# '.$command.PHP_EOL;
+    echo PHP_EOL.'# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
 
     $command = 'rm '.$publicDir.'/assets.tar.gz';
-    echo '# '.$command.PHP_EOL;
+    echo PHP_EOL.'# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
 
     $command = 'mv '.SS_DATABASE_NAME.'.sspak ..';
-    echo '# '.$command.PHP_EOL;
+    echo PHP_EOL.'# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
 
     echo '

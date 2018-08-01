638a77a82e25ae865d4f4e390a95a2d073f3df46
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-08-01 13:31:54 +1200

    FIXES

--- src/SSPAK.php
+++ src/SSPAK.php
@@ -13,17 +13,17 @@ if ((php_sapi_name() === 'cli')) {
         echo '---------- FAILED ------------';
     }
     if(file_exists($publicDir.'/public/assets')) {
-        $command = 'tar czf '.$cwd.'assets.tar.gz '.$publicDir.'/public/assets';
+        $command = 'tar czf --absolute-names '.$cwd.'assets.tar.gz '.$publicDir.'/public/assets';
         echo '# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
     elseif(file_exists($publicDir.'/assets')) {
-        $command = 'tar czf '.$cwd.'assets.tar.gz '.$publicDir.'/assets';
+        $command = 'tar czf --absolute-names '.$cwd.'assets.tar.gz '.$publicDir.'/assets';
         echo '# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
 
-    $command = 'tar cf '.$cwd.''.SS_DATABASE_NAME.'.sspak '.$cwd.'assets.tar.gz '.$cwd.'database.sql.gz';
+    $command = 'tar cf --absolute-names '.$cwd.''.SS_DATABASE_NAME.'.sspak '.$cwd.'assets.tar.gz '.$cwd.'database.sql.gz';
     echo '# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
     echo '

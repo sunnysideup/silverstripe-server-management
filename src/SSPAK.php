1706403fc690386cd7c9a172d851afff518b3640
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-08-01 13:41:50 +1200

    FIXES

--- src/SSPAK.php
+++ src/SSPAK.php
@@ -4,7 +4,7 @@ if ((php_sapi_name() === 'cli')) {
 
     require_once('dirs.php');
 
-    $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > '.$cwd.'database.sql.sql';
+    $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > '.$publicDir.'/database.sql.sql';
     echo '# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
     if (file_exists('/container/application/database.sql')) {
@@ -13,17 +13,25 @@ if ((php_sapi_name() === 'cli')) {
         echo '---------- FAILED ------------';
     }
     if(file_exists($publicDir.'/public/assets')) {
-        $command = 'cd '.$publicDir.' && tar czf assets.tar.gz '.$publicExtension.'/assets';
+        $command = 'cd '.$publicDir.' && tar czf assets.tar.gz '.'/public/assets';
         echo '# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
     elseif(file_exists($publicDir.'/assets')) {
-        $command = 'cd '.$cwd.' && tar czf assets.tar.gz assets';
+        $command = 'cd '.$publicDir.' && tar czf assets.tar.gz /assets';
         echo '# '.$command.PHP_EOL;
         var_dump(shell_exec($command));
     }
 
-    $command = 'cd '.$cwd.' && tar cf '.SS_DATABASE_NAME.'.sspak assets.tar.gz database.sql.gz';
+    $command = 'cd '.$publicDir.' && tar cf '.SS_DATABASE_NAME.'.sspak assets.tar.gz database.sql.gz';
+    echo '# '.$command.PHP_EOL;
+    var_dump(shell_exec($command));
+
+    $command = 'rm '.$publicDir.'/database.sql.sql';
+    echo '# '.$command.PHP_EOL;
+    var_dump(shell_exec($command));
+
+    $command = 'rm '.$publicDir.'/assets.tar.gz';
     echo '# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
     echo '

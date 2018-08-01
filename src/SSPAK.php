f6b92efabc0995ea86a41b7b11d027a18ae4abb8
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-08-01 13:50:04 +1200

    FIXES

--- src/SSPAK.php
+++ src/SSPAK.php
@@ -5,13 +5,13 @@ if ((php_sapi_name() === 'cli')) {
     require_once('dirs.php');
 
     $command = 'mysqldump -u '.SS_DATABASE_USERNAME.' -p'.SS_DATABASE_PASSWORD.' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' > '.$publicDir.'/database.sql';
-    echo '# '.$command.PHP_EOL;
+    echo PHP_EOL.'# '.$command.PHP_EOL;
     var_dump(shell_exec($command));
-    if (file_exists($publicDir.'/database.sql')) {
-        echo '---------- DUMPED ------------';
-    } else {
-        echo '---------- FAILED ------------';
-    }
+
+    $command = 'cd '.$publicDir.' && gzip database.sql';
+    echo PHP_EOL.'# '.$command.PHP_EOL;
+    var_dump(shell_exec($command));
+
     if(file_exists($publicDir.'/public/assets')) {
         $command = 'cd '.$publicDir.' && tar czf assets.tar.gz public/assets';
         echo PHP_EOL.'# '.$command.PHP_EOL;

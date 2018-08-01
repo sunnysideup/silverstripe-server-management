a9d10acd88824a69592bb683bfd986f1d4cb8a61
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-08-01 13:18:29 +1200

    MAJOR: new version

--- src/IMPORT_DB.php
+++ src/IMPORT_DB.php
@@ -1,9 +1,9 @@
 <?php
 if ((php_sapi_name() === 'cli')) {
-    $location =  '/container/application/_ss_environment.php';
+
+    require_once('dirs.php');
     
-    require_once($location);
-    $command = 'mysql -u '.SS_DATABASE_USERNAME.' -p\''.SS_DATABASE_PASSWORD.'\' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' < /container/application/'.SS_DATABASE_NAME.'.sql';
+    $command = 'mysql -u '.SS_DATABASE_USERNAME.' -p\''.SS_DATABASE_PASSWORD.'\' -h '.SS_DATABASE_SERVER.' '.SS_DATABASE_NAME.' < '.$cwd.''.SS_DATABASE_NAME.'.sql';
     $message   =  "Are you sure you want to do import a database [y/N]";
     print PHP_EOL;
     print PHP_EOL;
@@ -17,7 +17,7 @@ if ((php_sapi_name() === 'cli')) {
     if ( $confirmation !== 'y' ) {
        // The user did not say 'y'.
        exit (0);
-    } else {   
+    } else {
         var_dump(shell_exec($command));
         echo PHP_EOL;
         echo PHP_EOL;
